6e0e6b6f0cc6ea35a7865ae912543271a47bf065
tumiMsaki <245551699@qq.com>
2018-04-24 16:47:10 +0800

    第二次

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 window.onload = function() {
-    document.getElementById('uploadFile').onchange = function(event) {
+    document.querySelector('#uploadFile').addEventListener("change", function(event) {
         var file = event.target.files[0];
         var reader = new FileReader();
         reader.readAsDataURL(file);
         reader.onload = function(ev) {
             img_src = ev.target.result;
-            var imgs = document.getElementsByClassName('img');
+            var imgs = document.querySelectorAll('.img');
             imgs[0].setAttribute('src', img_src);
         }
-    };
-    document.getElementById('btn').onclick = function() {
+    })
+
+    document.querySelector('#btn').onclick = function() {
         $.ajax({
             url: "http://127.0.0.1:8888/upload",
             type: "POST",
@@ -20,19 +21,18 @@ window.onload = function() {
             error: function(XMLHttpRequest, textStatus, errorThrown) {
                 alert("error message : " + errorThrown.toString())
             },
+            xhr: function() { //用以显示上传进度  
+                var xhr = $.ajaxSettings.xhr();
+                if (xhr.upload) {
+                    xhr.upload.addEventListener('progress', function(event) {
+                        var percent = Math.floor(event.loaded / event.total * 100);
+                        document.querySelector("#progress .progress-item").style.width = percent + "%";
+                    }, false);
+                }
+                return xhr
+            },
             success: function() {
                 alert('1');
-
-                function progressFunction(evt) {
-                    var progressBar = document.getElementById("progressBar");
-                    var percentageDiv = document.getElementById("percentage");
-                    if (evt.lengthComputable) {
-                        progressBar.max = evt.total;
-                        progressBar.value = evt.loaded;
-                        percentageDiv.innerHTML = Math.round(evt.loaded / evt.total * 100) + "%";
-                    }
-                }
-                progressFunction();
             }
         })
     }
